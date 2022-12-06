43c1fe8a142e0eb510991930fc281836841510ab
Chris DiMartino <cdimartino@usertesting.com>
2022-12-06 00:42:52 -0500

    Refactor

--- 2022/06/decoder.rb
+++ 2022/06/decoder.rb
@@ -1,11 +1,12 @@
 input = File.open("input.txt").read.chomp.chars
 
-CONS = ARGV[0].to_i
+LENGTH = ARGV[0].to_i
+IDX_OFFSET = MIN_IDX = LENGTH - 1
 
 input.each.with_index do |char, idx|
-  next unless idx > CONS
-  range = input[idx - CONS..idx]
-  if range.uniq.length == CONS + 1
+  next unless idx >= MIN_IDX
+  range = input[idx - IDX_OFFSET..idx]
+  if range.uniq.length == LENGTH
     puts range.uniq.join
     puts idx + 1
     break
